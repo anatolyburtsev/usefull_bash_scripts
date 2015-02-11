cf7b67e37c3755ab4a393ccb26c5ec6a4af90073
anatoly burtsev <anatolyburtsev@yandex.ru>
2015-02-11 14:13:17 +0300

    Update clean_es_log.sh

--- clean_es_log.sh
+++ clean_es_log.sh
@@ -5,12 +5,13 @@ curator=`which curator`
 host=''
 log=/var/log/${0%.sh}/output.log
 
-if [ -e $curator ] || [ ! -x $curator ] ; then echo 'curator not found'; exit 1; fi
+if [ ! -x $curator ] ; then echo 'curator not found'; exit 1; fi
+touch $log
 if [ ! -w $log ]; then echo "log file $log not writable"; exit 1; fi
 
 nc -vz -w 1 $host 9200 >$log 2>&1
 if [ "$?" -ne "0" ]; then echo "elasticsearch at $host not accessable"; exit 1; fi
 
-$curator --host $host delete --older-than 90 --prefix host_log_ --timestring %Y%m%d 2>$log || echo "failed delete" && exit 1
-$curator --host $host close --older-than 31 --prefix host_log_ --timestring %Y%m%d 2>$log || echo "failed close" && exit 1
-$curator --host $host optimize --older-than 2 --max_num_segments 1 --prefix host_log_ --timestring %Y%m%d 2>$log || echo "failed optime" && exit 1
+$curator --host $host delete --older-than 90 --prefix host_log_ --timestring %Y%m%d 2>$log || (echo "failed delete" && exit 1)
+$curator --host $host close --older-than 33 --prefix host_log_ --timestring %Y%m%d 2>$log || (echo "failed close" && exit 1)
+$curator --host $host optimize --older-than 2 --max_num_segments 1 --prefix host_log_ --timestring %Y%m%d 2>$log || (echo "failed optime" && exit 1)
